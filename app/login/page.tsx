67eba6c2c684445980e085bf0feaa8b267e2e2d0
sllobera <sebastian@llobera.com.au>
2024-07-31 10:23:26 +1000

    opi

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -56,27 +56,27 @@ export default function Login({
     <>    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
        
         
-        <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+        <form className="flex-1 flex flex-col w-full justify-center gap-2 ">
           <label className="text-md" htmlFor="email">
             Email
           </label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className="rpx-4 py-2 bg-inherit border mb-6"
             name="email"
             placeholder="you@example.com"
             required />
-          <label className="text-md" htmlFor="password">
+          <label className="text-sm" htmlFor="password">
             Password
           </label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className="px-4 py-2 bg-inherit border mb-6"
             type="password"
             name="password"
             placeholder="••••••••"
             required />
           <SubmitButton
             formAction={signIn}
-            className="bg-blue-700 rounded-md px-4 py-2 text-foreground mb-2"
+            className="bg-blue-700 px-4 py-2 text-foreground mb-2"
             pendingText="Signing In..."
           >
             Sign In
