428cd9e414f75847f5f30ced60376956fc35c5cc
sllobera <sebastian@llobera.com.au>
2024-08-02 16:57:55 +1000

    opo

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -151,11 +151,9 @@ export default async function ProtectedPage() {
       {people.map((person) => (
         <div
           key={person.email}
-          className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+          className="relative flex border-l-4 border-blue-400 items-center space-x-3 rounded-lg border border-gray-300 bg-white px-2 py-1 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
         >
-          <div className="flex-shrink-0">
-            <img alt="" src={person.imageUrl} className="h-10 w-10 rounded-full" />
-          </div>
+         
           <div className="min-w-0 flex-1">
             <a href="#" className="focus:outline-none">
               <span aria-hidden="true" className="absolute inset-0" />
