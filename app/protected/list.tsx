9057a77e9003edebb6cc8b3204680fbf8b675f72
sllobera <sebastian@llobera.com.au>
2024-08-03 16:36:59 +1000

    pop

--- app/protected/list.tsx
+++ app/protected/list.tsx
@@ -8,10 +8,10 @@ return[
 
     <ul
       role="list"
-      className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
+      className="divide-y divide-gray-400 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 "
     >
       
-        <li key={name} className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
+        <li key={name} className="relative border-l-4 border-blue-500 flex justify-between gap-x-6 px-1 py-1 hover:bg-gray-50 sm:px-6">
           <div className="flex min-w-0 gap-x-4">
             
             <div className="min-w-0 flex-auto">
