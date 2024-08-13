df84f04ea2429249090bcc01bb97e86b0c3459b7
sllobera <sebastian@llobera.com.au>
2024-08-13 11:31:42 +1000

    oooo

--- app/protected/people.tsx
+++ app/protected/people.tsx
@@ -104,7 +104,7 @@ const [people, setPeople] = useState([] as any);
         <li key={index} className="flex justify-between gap-x-6 py-5">
           <div className="flex min-w-0 gap-x-4">
          
-            <div className="min-w-0 flex-auto">
+            <div className="min-w-0 flex-auto ml-5">
               <p className="text-sm font-semibold leading-6 text-gray-900">{person.first_name + " " +person.last_name}</p>
               <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
             </div>
@@ -117,10 +117,10 @@ const [people, setPeople] = useState([] as any);
               </p>
             ) : (
               <div className="mt-1 flex items-center gap-x-1.5">
-                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                <div className="flex-none rounded-full bg-emerald-500/20">
                   <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
                 </div>
-                <p className="text-xs leading-5 text-gray-500">Online</p>
+                <p className="text-xs leading-5 text-gray-500 pr-5">Online</p>
               </div>
             )}
           </div>
