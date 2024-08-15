34936610e92a2adf822189ce23547b44a1e78839
sllobera <sebastian@llobera.com.au>
2024-08-15 19:15:48 +1000

    kjj

--- app/protected/people.tsx
+++ app/protected/people.tsx
@@ -2,8 +2,10 @@ import { CircleIcon } from "@radix-ui/react-icons"
 import { BarsArrowUpIcon, PlusIcon, UsersIcon ,PlusCircleIcon} from '@heroicons/react/20/solid'
 import { JSXElementConstructor, Key, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react"
 import { createClient } from "@/utils/supabase/client";
+import React from 'react';
 
-  
+import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
 import Loader from './loader';
   
@@ -51,9 +53,10 @@ useEffect(() => {getData()},[])
 const [accid, setAcc] = useState(''); 
 const [people, setPeople] = useState([{}] as any); 
 const [selectedind, setIndex] = useState('');
+const [selected, setSelected] = useState(people)
   return (
     
-    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+    <div className="divide-y divide-gray-200   rounded-lg bg-white shadow">
     <div className="px-4 py-5 sm:px-6">
     <div>
       <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
@@ -94,36 +97,47 @@ const [selectedind, setIndex] = useState('');
 
     <button key={index} 
         type="button"
-        className="rounded-full ml-1 mt-1 bg-blue-600 px-2.5 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="rounded-full ml-1 mt-1 bg-blue-600 px-2.5 py-1 text-xs font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
       >
         {acc}
       </button>)):<Loader/>}
 
 </div>
-      <ul role="list" className="divide-y divide-gray-100">
-      {people.map((person: {
-        id: string; first_name: string; last_name: string; email: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; surname: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; lastSeen: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; lastSeenDateTime: string | undefined; 
-},id: string | null | undefined) => (
-        <li key={person.id} className="flex justify-between gap-x-6 py-5"  onClick={()=>{if(selectedind == person.id ){setIndex('') }else{setIndex(person.id) }}}>
-          <div className="flex min-w-0 gap-x-4">
-         
-            <div className="min-w-0 flex-auto ml-5">
-              <p className="text-sm font-semibold leading-6 text-gray-900 text-nowrap">{person.first_name + " " +person.last_name}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500 text-nowrap">{person.email}</p>
-            </div>
-          </div>
-          <div className=" s">
-           
-              <div className="px-4 py-5 sm:p-6"> 
+     
+   <div className="px-4 py-5 sm:px-6">
+   {people.length >0  &&
+   <Listbox value={selected} onChange={setSelected}>
+      <Label className="block text-sm font-medium leading-6 text-gray-900">Assigned Accouns</Label>
+      <div className="relative mt-2">
+        <ListboxButton className="relative  cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6">
+          <span className="block truncate">{selected.first_name?selected.first_name +" "+ selected.last_name:"Select user"}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
+          </span>
+        </ListboxButton>
 
+        <ListboxOptions
+          transition
+          className="absolute z-10 mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+        >
+          {people.length >0 ?people.map((person: { id: Key ,first_name: string,last_name:string }) => (
+            <ListboxOption
+              key={person.id}
+              value={person}
+              className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 data-[focus]:bg-blue-600 data-[focus]:text-white"
+            >
+              <span className="block truncate font-normal group-data-[selected]:font-semibold">{person.first_name + person.last_name} </span>
 
-</div>
-            
-          </div>
-        </li>
-      ))}
-    </ul>
-
+              <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-blue-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                <CheckIcon aria-hidden="true" className="h-5 w-5" />
+              </span>
+            </ListboxOption>
+          )):<Loader/>}
+        </ListboxOptions>
+      </div>
+    </Listbox>}
+    
+   </div>
 
 
 
